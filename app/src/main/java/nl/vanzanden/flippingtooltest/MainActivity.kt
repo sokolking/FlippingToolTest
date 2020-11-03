1cbe99aec5fac8881adab9d1fe3ac73d84383a5a
Alan Kudukhov <sokolking@gmail.com>
2020-11-03 16:35:20 +0300

    initial commit

---
+++ app/src/main/java/nl/vanzanden/flippingtooltest/MainActivity.kt
@@ -0,0 +1,12 @@
+package nl.vanzanden.flippingtooltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
